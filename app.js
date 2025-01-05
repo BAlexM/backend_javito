90f55ea3d7ffc6cb41ba7effe1edaa7325372087
BAlexM <75310437+BAlexM@users.noreply.github.com>
2025-01-05 23:15:19 +0100

    Add files via upload
    A struktúra(ez külön branch-ként is fent van a következő kettő dolog előttről még mivel ez előre láthatóan nem fog változni), az adatbázis meghívás, meg az app.js (ez még nem teljes)

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const { sequelize } = require('./models/Wish');
+const wishRoutes = require('./routes/wishRoutes');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+app.use('/api/wishes', wishRoutes);
+
+
+app.get('/', async (req, res) => {
+  const { Wish } = require('./models/Wish');
+  const wishes = await Wish.findAll({ order: [['createdAt', 'DESC']] });
+  res.render('index', { wishes });
+});
+
+
+
